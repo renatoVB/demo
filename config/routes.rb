c86c6d720605aa19f3228450087f8ef7c231d1ab
Renato varas becker <ren.varas@gmail.com>
2018-02-10 11:10:11 -0300

    Primer commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'pages/contact'
+
+  root 'pages#index'
+
+  get 'pages/index'
+
+  get 'pages/about'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
